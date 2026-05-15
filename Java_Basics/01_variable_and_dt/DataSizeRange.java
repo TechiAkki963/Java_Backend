42e05691df99ba992b1b4d29dd7bfe9c73d91adf
Akshay <paraba777@gmail.com>
2026-05-15 22:17:36 +0530

    JavaBasics -- DataSizeRange

---
+++ Java_Basics/01_variable_and_dt/DataSizeRange.java
@@ -0,0 +1,19 @@
+class DataSizeRange 
+{
+    public static void main(String args[]){
+
+        System.out.println(Integer.MIN_VALUE); // -2147483648
+        System.out.println(Integer.MAX_VALUE); // 2147483647
+        System.out.println(Integer.BYTES);     // 4
+
+
+        System.out.println(Byte.MIN_VALUE);  // -128
+        System.out.println(Byte.MAX_VALUE);  // 127
+        System.out.println(Byte.BYTES);      // 1
+
+
+        System.out.println(Float.MIN_VALUE);  // 1.4E-45
+        System.out.println(Float.MAX_VALUE);  // 3.4028235E38
+        System.out.println(Float.BYTES);      // 4
+    }
+}
