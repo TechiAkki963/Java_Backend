bf0a88b3e0b1eac1b5289984bda14876673f3b6c
Akshay <paraba777@gmail.com>
2026-05-20 02:25:41 +0530

    JavaBasics -- added String Directory -- Printing.java format updated

--- Java_Basics/01_variable_and_dt/String/Printing.java
+++ Java_Basics/01_variable_and_dt/String/Printing.java
@@ -6,5 +6,26 @@ public class Printing
 
         System.out.println("Sum of "+ x + " and "+ y + " is " +(x+y)); 
 
+
+        int a = 10;
+        float b = 12.56f;
+        char c = 'A';
+        String str = "Java Program";
+
+        System.out.printf("Hello %d %f %c World\n", a, b, c);
+        // %d for int
+        // %f for float
+        // %c for character
+
+        System.out.printf("Hello %s \n", str);
+        // %s for string
+
+        System.out.printf("%1$s %2$d %1$s", str , x);
+        // index 
+
+        float p = 3.14356768f;
+        System.out.printf("\n %6.2f",p);
+        // here 6 is the space occupied by the number and the empty space
+        // .2 is for the precision
     }
 }
