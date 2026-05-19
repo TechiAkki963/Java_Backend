c404b4dfdc71eb64baec3d53818e069bdb020231
Akshay <paraba777@gmail.com>
2026-05-20 02:30:52 +0530

    JavaBasics -- comment added in DataSizeRange.java file

--- Java_Basics/01_variable_and_dt/DataSizeRange.java
+++ Java_Basics/01_variable_and_dt/DataSizeRange.java
@@ -2,6 +2,8 @@ class DataSizeRange
 {
     public static void main(String args[]){
 
+        // to get this .MIN_VALUE or other values - use command - javap java.lang.Integer in CMD
+
         System.out.println(Integer.MIN_VALUE); // -2147483648
         System.out.println(Integer.MAX_VALUE); // 2147483647
         System.out.println(Integer.BYTES);     // 4
