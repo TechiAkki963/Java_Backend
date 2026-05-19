7b55ffd3f7d0e3bc766e02b61fa394dfb34e3385
Akshay <paraba777@gmail.com>
2026-05-20 01:33:01 +0530

    JavaBasics -- added String Directory -- Printing.java

---
+++ Java_Basics/01_variable_and_dt/String/Printing.java
@@ -0,0 +1,10 @@
+public class Printing
+{
+    public static void main(String args[]){
+
+        int x = 10, y = 20;
+
+        System.out.println("Sum of "+ x + " and "+ y + " is " +(x+y)); 
+
+    }
+}
