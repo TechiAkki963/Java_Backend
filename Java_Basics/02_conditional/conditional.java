5ba6b8023620bddb8ece9b5a55db8bc81fe6c315
Akshay <paraba777@gmail.com>
2026-06-03 19:05:31 +0530

    JavaBascis / conditional /Largest number

--- Java_Basics/02_conditional/conditional.java
+++ Java_Basics/02_conditional/conditional.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 class conditional
 {
     public static void main(String a[]){
@@ -13,5 +16,47 @@ class conditional
        }else{
         System.out.println(z);
        }
+
+
+
+       int salary = 25_000;
+       if (salary >= 10_000){
+         salary = salary + 1_000;
+       }else if (salary >= 15_000 ) {
+        salary = salary + 2_000;
+       }else{
+        System.out.println("Not Eleigible for bonus");
+       }
+       System.out.println(salary);
+
+
+
+      Scanner in = new Scanner(System.in);
+      int n = in.nextInt();
+
+      for (int num = 1; num <= n; num++){
+          System.out.println(num);
+       }
+
+
+
+    //    while loops
+
+    // int num1 = 1;
+
+    // while(num1 >=  5){
+    //     System.out.println("hello");
+    // }
+    // num1 += 1;
+
+
+    // do while
+
+    // int n1 =1;
+    // do {
+    //     System.out.println("try me!");
+    //     n1++;
+    // }while(n1 !=1);
+
     }
 }
