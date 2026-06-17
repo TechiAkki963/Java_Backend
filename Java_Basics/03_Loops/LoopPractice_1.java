bb3a9ffc8efc4b6d9cd8ccd28e2c2faeb14b1a91
Akshay <paraba777@gmail.com>
2026-06-18 00:30:10 +0530

    Java_Basics/03_Loops

---
+++ Java_Basics/03_Loops/LoopPractice_1.java
@@ -0,0 +1,23 @@
+class LoopPractice_1{
+
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i < 100){
+
+            System.out.println(i);
+            i=i*2;
+        }
+
+        do {
+            System.out.println(i);
+            i=i*2;
+        }while(i < 100);
+
+
+    }
+
+
+
+}
