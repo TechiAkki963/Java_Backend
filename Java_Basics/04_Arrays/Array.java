1c88cb036e0c126ebf600d5680f394ff28ecfb98
Akshay <paraba777@gmail.com>
2026-07-02 13:05:10 +0530

    04_Arrays/Exercises -- ArrayP_1_4

--- Java_Basics/04_Arrays/Array.java
+++ Java_Basics/04_Arrays/Array.java
@@ -3,10 +3,20 @@ class Array{
      
 
     int A[] = new int [5];
+    System.out.println(A.length); //5
 
-    
+    int B[] = {1,2,3,4,5};
+    // System.out.println(B.length);
 
+    B[2] = 15;
 
+    for (int i = 0; i < B.length; i++){
+        System.out.println(B[i]++);
+
+    }
+    for (int x: B){
+        System.out.println(x);
+    }
  }   
 
 }
